f5e9ffe8d7a8ef4c8374a1f7a1cd4c131d0c8ab1
Gulácsi András <guland@protonmail.com>
2024-09-06 00:26:14 +0200

    Update site owner constants

--- config/site_owner.php
+++ config/site_owner.php
@@ -2,12 +2,14 @@
 
 // Settings to do with the identification of the site owner
 define('WEBSITE_NAME', 'PHP-MD Blog');
-define('OUR_NAME', 'Your Company Name');
+define('OUR_NAME', 'PHP-MD\'s Blog');
 define('OUR_TELNUM', '+36201234567');
 define('OUR_ADDRESS', '');
 define('OUR_EMAIL_ADDRESS', 'gandras@passmail.net');
 define('DEFAULT_IMAGE', '/assets/images/static.jpg');
+define('GITHUB', 'https://github.com/webandras/php-md');
+define('AUTHOR', 'András Gulácsi');
 
 
 define('WEBSITE_DESCRIPTION',
-    'A PHP-based static site generator that uses pure PHP and Markdown. Deploy your website to any cheap shared hosting platforms.');
+    'A PHP-based static site generator that uses pure PHP and Markdown. Deploy your website to any shared hosting platforms.');
