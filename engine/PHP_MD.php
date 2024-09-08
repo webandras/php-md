6cd49ef299ed8b743fda9f944bd215b990a51864
Gulácsi András <guland@protonmail.com>
2024-09-08 21:49:39 +0200

    Refactor engine code, use parent class, add a trait for extending the code

--- engine/PHP_MD.php
+++ engine/PHP_MD.php
@@ -1,102 +1,21 @@
 <?php
 
-use League\CommonMark\Environment\Environment;
+require_once __DIR__.'/PHP_MD_Base.php';
+require_once __DIR__.'/PHP_MD_Trait.php';
+
 use League\CommonMark\Exception\CommonMarkException;
-use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
-use League\CommonMark\Extension\FrontMatter\FrontMatterExtension;
 use League\CommonMark\Extension\FrontMatter\Output\RenderedContentWithFrontMatter;
-use League\CommonMark\MarkdownConverter;
 
 /**
  * Class for generating HTML file output from parsed markdown files
  */
-class PHP_MD
+class PHP_MD extends PHP_MD_Base
 {
-    /**
-     * @var MarkdownConverter
-     */
-    private MarkdownConverter $converter;
-
-
-    /**
-     * @var array
-     */
-    public array $posts = [];
-
-
-    /**
-     * @var string
-     */
-    private string $root_dir;
-
+    use PHP_MD_Trait;
 
     public function __construct()
     {
-        // Define your configuration, if needed
-        $markdownConfig = [];
-
-        // Configure the Environment with all the CommonMark parsers/renderers
-        $environment = new Environment($markdownConfig);
-        $environment->addExtension(new CommonMarkCoreExtension());
-
-        // Add the extension
-        $environment->addExtension(new FrontMatterExtension());
-
-        // Instantiate the converter engine and start converting some Markdown!
-        $this->converter = new MarkdownConverter($environment);
-
-        $this->root_dir = dirname(__DIR__);
-    }
-
-
-    /**
-     * Grab the post content
-     *
-     * @param  RenderedContentWithFrontMatter  $result
-     *
-     * @return string
-     */
-    public function get_post_content(RenderedContentWithFrontMatter $result
-    ): string {
-        return $result->getContent();
-    }
-
-
-    /**
-     * Grab the front matter
-     *
-     * @param  RenderedContentWithFrontMatter  $result
-     *
-     * @return array
-     */
-    public function get_post_front_matter(RenderedContentWithFrontMatter $result
-    ): array {
-        return $result->getFrontMatter();
-    }
-
-
-    /**
-     * @param  string  $destination_directory
-     * @param  string  $content
-     * @param  string  $filePath
-     * @param  string  $fileName
-     *
-     * @return bool
-     */
-    public function save_html(
-        string $destination_directory,
-        string $content,
-        string $filePath = '',
-        string $fileName = 'index'
-    ): bool {
-        $filename             = ($filePath !== '') ? $this->get_filename($filePath) : $fileName;
-        $destination_filepath = $destination_directory.$filename.'.html';
-
-        if (!is_dir($destination_directory)) {
-            mkdir($destination_directory, 0755);
-        }
-
-        return file_put_contents($destination_filepath, $content) !== false;
+        parent::__construct();
     }
 
 
@@ -109,8 +28,8 @@ class PHP_MD
     public function generate_posts(string $language = DEFAULT_LANGUAGE, array $data = []): array|null
     {
         $this->posts           = [];
-        $source_directory      = $this->root_dir.'/posts/'.($language !== DEFAULT_LANGUAGE ? ($language.'/') : '');
-        $destination_directory = $this->root_dir.'/public/posts/'.($language !== DEFAULT_LANGUAGE ? ($language.'/') : '');
+        $source_directory      = $this->root_dir.'/posts/'.$this->get_language_segment($language);
+        $destination_directory = $this->root_dir.'/public/posts/'.$this->get_language_segment($language);
         $files                 = glob($source_directory.'/*.md');
 
         // Read all markdown files and convert them to html
@@ -134,14 +53,9 @@ class PHP_MD
                 $this->posts[]       = $frontMatter;
                 $data['frontmatter'] = $frontMatter;
                 $data['content']     = $this->get_post_content($result);
-                $data['page_name']   = 'post';
-                $data['root_dir']    = $this->root_dir;
-                extract($data);
+                $data['template_name']   = 'single';
 
-                ob_start();
-                require $this->root_dir.'/templates/views/single.php';
-                $content = ob_get_contents();
-                ob_end_clean();
+                $content = $this->render_view_and_return($data);
 
                 $this->save_html($destination_directory, $content, $filepath);
             }
@@ -159,19 +73,15 @@ class PHP_MD
      */
     public function generate_index_page(string $language = DEFAULT_LANGUAGE, array $data = []): void
     {
-        $destination_directory = $this->root_dir.'/public/'.($language !== DEFAULT_LANGUAGE ? ($language.'/') : '');
+        $destination_directory = $this->root_dir.'/public/'.$this->get_language_segment($language);
         $data['posts']         = $this->posts;
-        $data['page_name']     = 'index';
-        $data['root_dir']      = $this->root_dir;
-        extract($data);
+        $data['template_name'] = 'index';
 
-        ob_start();
-        require $this->root_dir.'/templates/views/index.php';
-        $content = ob_get_contents();
-        ob_end_clean();
+        $content = $this->render_view_and_return($data);
 
         $this->save_html($destination_directory, $content);
     }
+
 
     /**
      * @param  string  $language
@@ -181,58 +91,13 @@ class PHP_MD
      */
     public function generate_archive_page(string $language = DEFAULT_LANGUAGE, array $data = []): void
     {
-        $destination_directory = $this->root_dir.'/public/'.($language !== DEFAULT_LANGUAGE ? ($language.'/') : '');
+        $destination_directory = $this->root_dir.'/public/'.$this->get_language_segment($language);
         $data['posts']                 = $this->posts;
-        $data['page_name']             = 'archive';
-        $data['root_dir']              = $this->root_dir;
-        extract($data);
+        $data['template_name']         = 'archive';
 
-        ob_start();
-        require $this->root_dir.'/templates/views/archive.php';
-        $content = ob_get_contents();
-        ob_end_clean();
+        $content = $this->render_view_and_return($data);
 
-        $this->save_html($destination_directory, $content, '', $page_name);
-    }
-
-
-    /**
-     * @param  string  $filePath
-     *
-     * @return string
-     */
-    private function get_filename(string $filePath): string
-    {
-        $parts = explode('/', $filePath);
-
-        return substr($parts[sizeof($parts) - 1], 0, -3);
-    }
-
-
-    /**
-     * @param  array  $front_matter
-     * @param  string  $filepath
-     * @param  string  $language
-     *
-     * @return array|void
-     */
-    private function transform_front_matter(
-        array $front_matter,
-        string $filepath,
-        string $language
-    ) {
-        $front_matter['slug'] = BASE_URL.'posts/'.($language !== DEFAULT_LANGUAGE ? ($language.'/') : '').$this->get_filename($filepath).'.html';
-        $date = $front_matter['date'].':00';
-
-        try {
-            $dt = new DateTime($date, new DateTimeZone(DEFAULT_TIMEZONE));
-            $front_matter['date'] = $dt->format(DEFAULT_DATE_FORMAT);
-        } catch (Exception $ex) {
-            var_dump($ex);
-            die;
-        }
-
-        return $front_matter;
+        $this->save_html($destination_directory, $content, '', $data['template_name']);
     }
 
 }
---
+++ engine/PHP_MD_Base.php
@@ -0,0 +1,173 @@
+<?php
+
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\FrontMatter\FrontMatterExtension;
+use League\CommonMark\Extension\FrontMatter\Output\RenderedContentWithFrontMatter;
+use League\CommonMark\MarkdownConverter;
+
+class PHP_MD_Base
+{
+    /**
+     * @var MarkdownConverter
+     */
+    protected MarkdownConverter $converter;
+
+
+    /**
+     * @var array
+     */
+    protected array $posts = [];
+
+
+    /**
+     * @var string
+     */
+    protected string $root_dir;
+
+
+    public function __construct() {
+        // Define your configuration, if needed
+        $markdownConfig = [];
+
+        // Configure the Environment with all the CommonMark parsers/renderers
+        $environment = new Environment($markdownConfig);
+        $environment->addExtension(new CommonMarkCoreExtension());
+
+        // Add the extension
+        $environment->addExtension(new FrontMatterExtension());
+
+        // Instantiate the converter engine and start converting some Markdown!
+        $this->converter = new MarkdownConverter($environment);
+
+        $this->root_dir = dirname(__DIR__);
+    }
+
+
+    /**
+     * Grab the post content
+     *
+     * @param  RenderedContentWithFrontMatter  $result
+     *
+     * @return string
+     */
+    protected function get_post_content(RenderedContentWithFrontMatter $result
+    ): string {
+        return $result->getContent();
+    }
+
+
+    /**
+     * Grab the front matter
+     *
+     * @param  RenderedContentWithFrontMatter  $result
+     *
+     * @return array
+     */
+    protected function get_post_front_matter(RenderedContentWithFrontMatter $result
+    ): array {
+        return $result->getFrontMatter();
+    }
+
+
+    /**
+     * @param  array  $front_matter
+     * @param  string  $filepath
+     * @param  string  $language
+     *
+     * @return array|void
+     */
+    protected function transform_front_matter(
+        array $front_matter,
+        string $filepath,
+        string $language
+    ) {
+        $front_matter['slug'] = BASE_URL.'posts/'. $this->get_language_segment($language) . $this->get_filename($filepath).'.html';
+        $date = $front_matter['date'].':00';
+
+        try {
+            $dt = new DateTime($date, new DateTimeZone(DEFAULT_TIMEZONE));
+            $front_matter['date'] = $dt->format(DEFAULT_DATE_FORMAT);
+        } catch (Exception $ex) {
+            var_dump($ex);
+            die;
+        }
+
+        return $front_matter;
+    }
+
+
+    /**
+     * @param  string  $language
+     *
+     * @return string
+     */
+    protected function get_language_segment(string $language): string
+    {
+        return $language !== DEFAULT_LANGUAGE ? ($language.'/') : '';
+    }
+
+
+    /**
+     * Gets a filename from a file path
+     *
+     * @param  string  $filePath
+     *
+     * @return string
+     */
+    protected function get_filename(string $filePath): string
+    {
+        $parts = explode('/', $filePath);
+
+        return substr($parts[sizeof($parts) - 1], 0, -3);
+    }
+
+
+    /**
+     * Saves file to a specified location, and with a specified filename
+     *
+     * @param  string  $destination_directory
+     * @param  string  $content
+     * @param  string  $filePath
+     * @param  string  $fileName
+     *
+     * @return bool
+     */
+    protected function save_html(
+        string $destination_directory,
+        string $content,
+        string $filePath = '',
+        string $fileName = 'index'
+    ): bool {
+        $filename             = ($filePath !== '') ? $this->get_filename($filePath) : $fileName;
+        $destination_filepath = $destination_directory.$filename.'.html';
+
+        if (!is_dir($destination_directory)) {
+            mkdir($destination_directory, 0755);
+        }
+
+        return file_put_contents($destination_filepath, $content) !== false;
+    }
+
+
+    /**
+     * Renders the view with the data passed to it, and returns output buffer content
+     *
+     * @param  array  $data
+     *
+     * @return string
+     */
+    protected function render_view_and_return(array $data): string {
+        $data['root_dir'] = $this->root_dir;
+        extract($data);
+
+        ob_start();
+        require $this->root_dir.'/templates/views/' . $template_name . '.php';
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+
+
+}
